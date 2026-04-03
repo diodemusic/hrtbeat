0052ac4390fadeb222f55d0fbf0a2c603fe7082d
diodemusic <diodemusicuk@gmail.com>
2026-04-03 21:28:12 +0100

    Add username field

--- backend/src/models/users.py
+++ backend/src/models/users.py
@@ -9,6 +9,7 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str]
     email: Mapped[Optional[str]]
     mobile_number: Mapped[Optional[str]]
 
