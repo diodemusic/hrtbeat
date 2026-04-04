a1d387e61b88a38fc3580e223b7b1c9696fff667
diodemusic <diodemusicuk@gmail.com>
2026-04-04 23:55:19 +0100

    Create get site_watches endpoint

--- backend/src/api/api.py
+++ backend/src/api/api.py
@@ -8,6 +8,8 @@ from ..core.tables import create_tables
 from ..models.site_watches import SiteWatch
 from ..models.sites import Site
 
+USER_ID = 1
+
 create_tables()
 
 
@@ -24,8 +26,6 @@ class DeleteSiteRequest(BaseModel):
 
 @app.post("/site", status_code=201)
 def add_site(addSiteRequest: AddSiteRequest):
-    user_id = 1
-
     def add_site_watch(session, user_id, site_id) -> dict[str, int]:
         site_watch_object = SiteWatch(user_id=user_id, site_id=site_id)
         session.add(site_watch_object)
@@ -39,7 +39,7 @@ def add_site(addSiteRequest: AddSiteRequest):
 
         if site:
             stmt = select(SiteWatch).where(
-                SiteWatch.user_id == user_id, SiteWatch.site_id == site.id
+                SiteWatch.user_id == USER_ID, SiteWatch.site_id == site.id
             )
             site_watch = session.execute(stmt).scalars().first()
 
@@ -49,7 +49,7 @@ def add_site(addSiteRequest: AddSiteRequest):
                     detail=f"Conflict: {site_watch}",
                 )
             else:
-                r = add_site_watch(session=session, user_id=user_id, site_id=site.id)
+                r = add_site_watch(session=session, user_id=USER_ID, site_id=site.id)
                 session.commit()
 
                 return r
@@ -57,7 +57,7 @@ def add_site(addSiteRequest: AddSiteRequest):
             site_object = Site(url=addSiteRequest.url)
             session.add(site_object)
             session.flush()
-            r = add_site_watch(session=session, user_id=user_id, site_id=site_object.id)
+            r = add_site_watch(session=session, user_id=USER_ID, site_id=site_object.id)
             session.commit()
 
             return r
@@ -65,11 +65,9 @@ def add_site(addSiteRequest: AddSiteRequest):
 
 @app.delete("/site-watch", status_code=204)
 def delete_site_watch(deleteSiteRequest: DeleteSiteRequest):
-    user_id = 2
-
     with Session(engine) as session:
         stmt = select(SiteWatch).where(
-            SiteWatch.id == deleteSiteRequest.id, SiteWatch.user_id == user_id
+            SiteWatch.id == deleteSiteRequest.id, SiteWatch.user_id == USER_ID
         )
         site_watch = session.execute(stmt).scalars().first()
 
@@ -80,7 +78,7 @@ def delete_site_watch(deleteSiteRequest: DeleteSiteRequest):
             )
 
         session.delete(site_watch)
-        session.flush
+        session.flush()
 
         stmt = select(SiteWatch).where(SiteWatch.site_id == site_watch.site_id)
         other_site_watch = session.execute(stmt).scalars().first()
@@ -92,3 +90,28 @@ def delete_site_watch(deleteSiteRequest: DeleteSiteRequest):
             session.delete(site)
 
         session.commit()
+
+
+@app.get("/site-watches", status_code=200)
+def get_site_watches():
+    with Session(engine) as session:
+        stmt = select(SiteWatch).where(SiteWatch.user_id == USER_ID)
+        site_watches = session.execute(stmt).scalars()
+
+        response = []
+
+        for site_watch in site_watches:
+            stmt = select(Site).where(Site.id == site_watch.site_id)
+            site = session.execute(stmt).scalars().first()
+            response.append(
+                {
+                    "url": site.url,
+                    "status": site.status,
+                    "pings": [
+                        {"latency": 20, "timestamp": "2026-04-04T12:00:00"},
+                        {"latency": 35, "timestamp": "2026-04-04T12:05:00"},
+                    ],
+                }
+            )
+
+        return response
