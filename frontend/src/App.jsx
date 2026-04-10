a471b86d1cb49881f6d7d5654f15eeeff172a138
diodemusic <diodemusicuk@gmail.com>
2026-04-10 17:43:56 +0100

    Add site-watch functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [url, setUrl] = useState("");
+
+  async function watchNewSite() {
+    const response = await fetch("http://127.0.0.1:8000/site-watch", {
+      method: "POST",
+      body: JSON.stringify({ url: url }),
+      headers: { "Content-Type": "application/json" }
+    });
+    setUrl("");
+    console.log(response);
+  }
 
   return (
     <>
-      <h1>hellor</h1>
+      <div>
+        <h1>hellor</h1>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <button type="submit" onClick={watchNewSite}>
+          Add Site
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
